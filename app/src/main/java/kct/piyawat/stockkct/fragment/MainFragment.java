dc4c08a556a353d102f0a50d98fdf8ead29c4add
MasterUNG <phrombutr@gmail.com>
2017-09-26 16:02:07 +0700

    Finish 26Sec

---
+++ app/src/main/java/kct/piyawat/stockkct/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package kct.piyawat.stockkct.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kct.piyawat.stockkct.R;
+
+/**
+ * Created by masterung on 9/26/2017 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}    //Main Class
